65ef52b2c19f3cd318ff1f32eeeb9baac67a8161
Mirjana <mirjana.stojanova@students.finki.ukim.mk>
2024-04-01 17:29:17 +0200

    Styling changes of Input component

--- components/custom/Input.js
+++ components/custom/Input.js
@@ -1,14 +1,27 @@
 import styled from "styled-components";
 
 const InputStyle = styled.input`
-  width: 100%;
+  width: 95%;
   border: 1px solid #999;
   border-radius: 5px;
   padding: 10px;
+  margin: 0;
+  @media screen and (min-width: 768px) {
+    width: 100%;
+    border: 1px solid #999;
+    border-radius: 5px;
+    padding: 10px;
+  }
 `;
 
 const Input = (props) => {
-  return <InputStyle type="text" placeholder="Пронајди производ" onChange={props.search}/>;
+  return (
+    <InputStyle
+      type="text"
+      placeholder="Пронајди производ"
+      onChange={props.search}
+    />
+  );
 };
 
 export default Input;
